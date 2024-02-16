5b8566e5c1bb0707091fc53bfb0e6c0393657ed4
Jorge Mateus <jorgemateus1995@gmail.com>
2024-02-16 10:09:00 -0500

    Se actualiza el formulario para mostrar una alerta personalizada al momento de enviarlo

--- script.js
+++ script.js
@@ -5,25 +5,45 @@ allLinks.forEach(function (link) {
     e.preventDefault();
     const href = link.getAttribute("href");
 
-    // Scroll back to top
+    // Subir al inicio
     if (href === "#")
       window.scrollTo({
         top: 0,
         behavior: "smooth",
       });
 
-    // Scroll to other links
+    // Navegar entre secciones
     if (href !== "#" && href.startsWith("#")) {
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({ behavior: "smooth" });
     }
 
+    //Manejar links a paginas externas
     if (href !== "#" && !href.startsWith("#") && href.startsWith("http")) {
       window.location.href = href;
     }
+  });
+});
 
-    // Close mobile naviagtion
-    if (link.classList.contains("main-nav-link"))
-      headerEl.classList.toggle("nav-open");
+//Para atrapar las respuestas del formulario
+
+window.addEventListener("load", function () {
+  const form = document.getElementById("my-form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: "POST",
+      body: data,
+    }).then(() => {
+      document.getElementById("nombre").value = "";
+      document.getElementById("correo").value = "";
+      document.getElementById("where").value =
+        document.getElementById("where").options[0].value;
+      document.getElementById("pago").value =
+        document.getElementById("pago").options[0].value;
+      alert("Formulario enviado exitosamente");
+    });
   });
 });
